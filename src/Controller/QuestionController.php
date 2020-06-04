7558105842f5e787043b06fa9f58522d595d0216
beatap123 <putkowska.b@gmail.com>
2020-06-04 16:03:31 +0200

    new template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,15 +1,29 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
-	/**
-	*@Route("/")
+     /**
+	*@Route("/") 
 	*/
-   
+	
+	public function searching()
+	{
+                return $this->render('question/show.html.twig',[
+                    'question'=> 'question',
+                    'answers' => 'answers',
+               ]);
+	}
+
+    
+	/**
+	*@Route("/szukaj") //dodać 'przepis?
+	*/
+        
 	public function curl_get($url="https://www.kwestiasmaku.com/szukaj?", array $get = array('search_api_views_fulltext' => 'beza'), array $options = array())
         
         {  
@@ -30,20 +44,10 @@ class QuestionController
             curl_close($ch);
             
             return new Response($result);
+            //'answers' => $answers, to sie przyda to umieszczenia $przepis w array
             
         }
 	
+       
+        
 }
-
-
-        /**
-	*@Route("/questions/{slug}") 
-	*/
-	
-	//public function show($slug)
-	//{
-		//return new Response(sprintf(
-		//'Future page to show a question "%s"!',
-		//ucwords(str_replace('-',' ',$slug))
-		//));
-	//}
