a46968ce7c0115f6d7229931b2221e56101e64af
beatap123 <putkowska.b@gmail.com>
2020-06-12 16:25:21 +0200

    add asset package

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Asset\Package;
 
 class QuestionController extends AbstractController
 {
@@ -46,17 +47,52 @@ class QuestionController extends AbstractController
             // $content = '{"id":521583, "name":"symfony-docs", ...}
             
             echo "Poniżej znajdują się wyniki wyszukiwania hasła '$przepis'";
-            echo "<table BORDER width='47%' cellspacing='0' cellpadding='0'>
-                    <tr><td width='100'>$content</td></tr>
-                    </table>";
-            //return new Response($content);
+           //return new Response($content);
+           }
+           
+           
+           
+           /**
+            *@Route("/szukaj/cos")
+            */
+
+            public function getURL()
+            { 
+            $przepis = $_POST['przepis'];
+            //$url="https://www.kwestiasmaku.com/szukaj?";
+            //$get = array('search_api_views_fulltext' => $przepis);
+
+            
+            $client = HttpClient::create();
+            // it makes an HTTP GET request to https://httpbin.org/get?token=...&name=...
+            $response = $client->request('GET', 'https://www.kwestiasmaku.com/szukaj?', [
+                'query' => [
+                    'search_api_views_fulltext' => $przepis,
+                ],
+            ]);
+            
+            
+            $statusCode = $response->getStatusCode();
+            // $statusCode = 200
+            $contentType = $response->getHeaders()['content-type'][0];
+            // $contentType = 'application/json'
+            $content = $response->getContent();
+            $name = "Frame";
+            $koniec = $this->getURL($content,$name,"GET"); 
+            
+            
+            return $this->render('question/curl.html.twig',
+                    ['result'=>$koniec]
+            ); 
+            }
+            //
     
 
             /*return $this->render('question/curl.html.twig',
                     ['result'=>$response*/
               //);                            
                         
-        }
+        
 	
        
         
