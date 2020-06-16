5e2c277e06f5be8a4ebad3f421bc612f69e9050c
beatap123 <putkowska.b@gmail.com>
2020-06-16 16:05:00 +0200

    new project

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -22,7 +22,29 @@ class QuestionController extends AbstractController
 	*@Route("/szukaj")
 	*/
         
-	
+        public function bus_stops()
+        {    
+        $przystanek = $_POST['przystanek'];
+            //$url=https://api.um.warszawa.pl/api/action/dbstore_get?id=ab75c33d-3a26-4342-b36a-6e5fef0a3ac3&sortBy=id&apikey=wartość
+            //$get = array('search_api_views_fulltext' => $przepis);
+
+            
+            $client = HttpClient::create();
+            // it makes an HTTP GET request to https://httpbin.org/get?token=...&name=...
+            $response = $client->request('GET', 'https://api.um.warszawa.pl/api/action/dbstore_get'
+           );
+            
+            $statusCode = $response->getStatusCode();
+            // $statusCode = 200
+            $contentType = $response->getHeaders()['content-type'][0];
+            // $contentType = 'application/json'
+            $content = $response->getContent();
+            // $content = '{"id":521583, "name":"symfony-docs", ...}
+             
+           return new Response($content);
+        /*
+        
+	*/
            
             /*return $this->render('question/curl.html.twig',
                     ['result'=>new Response($content)]
