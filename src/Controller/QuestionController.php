0cc5f8e267847ac36b5322f885b264b49688e90f
beatap123 <putkowska.b@gmail.com>
2020-06-18 08:10:38 +0200

    added more parameters to request

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -24,18 +24,27 @@ class QuestionController extends AbstractController
         
         public function bus_stops()
         {    
-        $przystanek = $_POST['przystanek'];
-        //APIKey: a054cce1-28f7-4f3d-9b06-7d682f3bd9b6
+        $nazwaprzystanku = $_POST['przystanek'];
+        //APIKey: a054cce1-28f7-4f3d-9b06-7d682f3bd9b6 -> dodać za pomocą odpowiedniej funkcji
 
 
-            $client = HttpClient::createForBaseUri('https://api.um.warszawa.pl/api/action/dbtimetable_get', 
+            $client = HttpClient::createForBaseUri('https://api.um.warszawa.pl/api/action/dbtimetable_get?id=b27f4c17-5c50-4a5b-89dd-236b282bc499&name=nazwaprzystanku&apikey=wartość', 
                     [
-                    'auth_basic' => ['beatap', 'bex8zcFauQLRmJv']
+                    'auth_basic' => ['beatap', 'bex8zcFauQLRmJv'],
+                    'query' => [
+                            'apikey' => 'a054cce1-28f7-4f3d-9b06-7d682f3bd9b6',
+                            'name' => $nazwaprzystanku
+                        ]    
             ]);
-            $response = $client->request('GET', 'https://api.um.warszawa.pl/api/action/dbtimetable_get', [
-            // use a different HTTP Basic authentication only for this request
-            'auth_basic' => ['beatap', 'bex8zcFauQLRmJv']
-             ]);
+            $response = $client->request('GET', 'https://api.um.warszawa.pl/api/action/dbtimetable_get?id=b27f4c17-5c50-4a5b-89dd-236b282bc499&name=nazwaprzystanku&apikey=wartość', 
+           [
+                    'auth_basic' => ['beatap', 'bex8zcFauQLRmJv'],
+                    'query' => [
+                            'apikey' => 'a054cce1-28f7-4f3d-9b06-7d682f3bd9b6',
+                            'name' => $nazwaprzystanku
+                        ]
+            ]);
+            
             $statusCode = $response->getStatusCode();
             // $statusCode = 200
             $contentType = $response->getHeaders()['content-type'][0];
