4fbf38c19d6b326dd5adabedd27f40aac3a3f5e3
beatap123 <putkowska.b@gmail.com>
2020-06-24 18:59:54 +0200

    add new api

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -29,7 +29,7 @@ class QuestionController extends AbstractController
 
             $client = HttpClient::create();
             
-            $response = $client->request('GET','https://api.um.warszawa.pl/api/action/wfsstore_get?id=a08136ec-1037-4029-9aa5-b0d0ee0b9d88&circle=21.02,52.21,1000&apikey=a054cce1-28f7-4f3d-9b06-7d682f3bd9b6', 
+            $response = $client->request('GET','https://api.um.warszawa.pl/api/action/wfsstore_get?id=a08136ec-1037-4029-9aa5-b0d0ee0b9d88&circle=21.02,52.21,1000', 
            [
                     'auth_basic' => ['beatap', 'bex8zcFauQLRmJv'],
                     'query' => [
@@ -56,6 +56,34 @@ class QuestionController extends AbstractController
                         
         }
 	
-       
+       /**
+	*@Route("/pogoda")
+	*/
+        
+        public function weather_api()
+                
+        {
+            //APIKey: 360938ff9c895d925dc406829c29353a -> dodać za pomocą odpowiedniej funkcji
+
+            $client = HttpClient::create();
+            
+            $response = $client->request('GET','http://samples.openweathermap.org/data/2.5/weather?q=London,uk', 
+           
+                    [        'auth_basic' => ['beatap', 'H7REvQ3pXiM3Xnc'],
+                    'query' => [
+                            'appid' => '360938ff9c895d925dc406829c29353a'
+                            //'name' => $nazwaprzystanku JEST PROBLEM Z MAP SERVER
+                        ]   
+            ]);
+            
+            $statusCode = $response->getStatusCode();
+            // $statusCode = 200
+            $contentType = $response->getHeaders()['content-type'][0];
+            // $contentType = 'application/json'
+            $content = $response->getContent();
+            // $content = '{"id":521583, "name":"symfony-docs", ...}
+             
+           return new Response($content);
+        }
         
 }
