7fdb90345b4465f193468928e10691331cd7ec92
beatap123 <putkowska.b@gmail.com>
2020-06-30 11:04:29 +0200

    connect with database

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -22,32 +22,11 @@ class QuestionController extends AbstractController
 	*@Route("/szukaj")
 	*/
         
-        public function bus_stops()
+        public function searching_street()
         {    
-        //APIKey: a054cce1-28f7-4f3d-9b06-7d682f3bd9b6 -> dodać za pomocą odpowiedniej funkcji
-
-            $client = HttpClient::create();
-            
-            $response = $client->request('GET','https://api.dane.gov.pl/datasets/469,adresy-mst-warszawy', 
-           [
-                    'auth_basic' => ['beatap', 'bex8zcFauQLRmJv'],
-                    'query' => [
-                            //'apikey' => 'a054cce1-28f7-4f3d-9b06-7d682f3bd9b6'
-                            'adres' => 'Mokotowska'
-                        ]   
-            ]);
-            
-            $statusCode = $response->getStatusCode();
-            // $statusCode = 200
-            $contentType = $response->getHeaders()['content-type'][0];
-            // $contentType = 'application/json'
-            $content = $response->getContent();
-            // $content = '{"id":521583, "name":"symfony-docs", ...}
-             
-           return new Response($content);
-        /*
+  
         
-	*/
+      
            
             /*return $this->render('question/curl.html.twig',
                     ['result'=>new Response($content)]
